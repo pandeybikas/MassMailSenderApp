23391f1db78abd9ec0df4cdb3079910e23125bce
Bikas Pandey <43566436+pandeybikas@users.noreply.github.com>
2023-06-01 10:00:41 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bikaspandey92@gmail.com'
-EMAIL_HOST_PASSWORD = 'rkmpoenfzflfieri'
+EMAIL_HOST_USER = 'put your email username here'
+EMAIL_HOST_PASSWORD = 'our email password goes here'
 EMAIL_USE_TLS = True
 
